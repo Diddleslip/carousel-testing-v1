47576cd7c51358edf16dc3249418a52c8af7290e
Mario Fernandez <diddleslip@gmail.com>
2020-09-17 09:46:23 -0400

    Added two carousel examples, second one works intended.

--- carousel-testing/src/App.js
+++ carousel-testing/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Carousel from "./components/Carousel/Carousel";
+import SecondCarousel from "./components/SecondCarousel/SecondCarousel";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from Carousel App.js!</h1>
+      <h1>Hello World</h1>
+      {/* <Carousel/> */}
+      <SecondCarousel/>
     </div>
   );
 }
