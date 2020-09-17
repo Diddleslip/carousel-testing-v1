c94276c6774996c91c9493186024aae7c256f7cf
Mario Fernandez <diddleslip@gmail.com>
2020-09-17 11:04:02 -0400

    Reformatted SeconCarousel's class -like component into a functional-like component and renaming it ThirdCarousel.

---
+++ carousel-testing/src/components/ThirdCarousel/ThirdCarousel.js
@@ -0,0 +1,47 @@
+import React, { useState, useRef } from 'react';
+import AliceCarousel from 'react-alice-carousel';
+
+import 'react-alice-carousel/lib/alice-carousel.css';
+import "./ThirdCarousel.css";
+
+const items = ["https://i.imgur.com/VTpz8XB.jpg", "https://i.imgur.com/nff0PWy.jpg", "https://i.imgur.com/fY311wc.jpeg", "https://i.imgur.com/BaGeu7F.jpeg", "https://i.imgur.com/rc9nH0R.jpeg", "https://i.imgur.com/xnoLgTy.jpeg"];
+
+function ThirdCarousel() {
+  let Carousel = useRef();
+  
+  const galleryItems = items.map((image, index) => (
+    <img className="mainImages" key={index} src={image} ></img>
+  ))
+
+  const thumbItem = (item, index) => (
+    <img 
+      key={item} 
+      onClick={() => {
+        Carousel.slideTo(index)
+      }} 
+      src={item}
+      className="thumbImages"
+    />
+  )
+
+  return (
+    <div>
+      <AliceCarousel
+        dotsDisabled={true}
+        buttonsDisabled={true}
+        items={galleryItems}
+        mouseTrackingEnabled
+        ref={el => Carousel = el}
+      />
+
+      <nav>{items.map((item, index) => (
+        thumbItem(item, index)
+      ))}
+      </nav>
+      <button onClick={() => Carousel.slidePrev()}>Prev image</button>
+      <button onClick={() => Carousel.slideNext()}>Next image</button>
+    </div>
+  )
+}
+
+export default ThirdCarousel
